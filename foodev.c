c2a8b1e979a12e397c45c206d52c00b3896574d3
Dave Reisner <d@falconindy.com>
2010-04-26 17:23:55 -0400

    clean up comments

--- foodev.c
+++ foodev.c
@@ -15,12 +15,9 @@ int main(void) {
   /* create contexts */
   udev = udev_new();
   monitor = udev_monitor_new_from_netlink(udev, "udev");
-  /* enumerate = udev_enumerate_new(udev); */
 
   /* Listen for events */
   udev_monitor_filter_add_match_subsystem_devtype(monitor, "usb", NULL); 
-
-  /* Listen to events */
   udev_monitor_enable_receiving(monitor);
 
   printf("Entering poll loop -- ^D to end\n");
@@ -32,19 +29,22 @@ int main(void) {
     pfd[0].fd = udev_monitor_get_fd(monitor);
     pfd[0].events = POLLIN;
 
-
-    /* poll on STDIN so we can quit */
+    /* poll on STDIN so we can quit on key input */
     pfd[1].fd = STDIN_FILENO;
     pfd[1].events = POLLIN;
 
-    /* got nothin', keep going */
+    /* block until we get an event */
     if (poll(pfd, 2, -1) < 1)
       continue;
 
+    /* If we're here, we received an event. Find out who sent it by checking
+     * each struct pollfd for revents
+     */
+
     /* found a udev event */
     if (pfd[0].revents & POLLIN) {
-
       device = udev_monitor_receive_device(monitor);
+
       if (device == NULL) {
         fprintf(stderr, "DEBUG: device == NULL\n");
         continue;
@@ -59,7 +59,7 @@ int main(void) {
       udev_device_unref(device);
     }
 
-    /* console input, we're done  */
+    /* input on STDIN, we're done  */
     if (pfd[1].revents & POLLIN)
       break;
   }
@@ -73,7 +73,8 @@ int main(void) {
 }
 
 void udev_device_add_event(struct udev_device *device) {
-  printf("in udev_device_add_event\n");
+  printf("in udev_device_add_event\n"); /* hello! */
+
   const char *product = udev_device_get_sysattr_value(device, "product");
   const char *driver = udev_device_get_driver(device);
   const char *devnode = udev_device_get_devnode(device);
@@ -85,5 +86,5 @@ void udev_device_add_event(struct udev_device *device) {
 }
 
 void udev_device_remove_event(struct udev_device *device) {
-  printf("in udev_device_remove_event\n");
+  printf("in udev_device_remove_event\n"); /* hello! */
 }
